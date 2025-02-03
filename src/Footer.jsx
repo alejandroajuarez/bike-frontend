9183f89ae0399c026c5885dfbc40ef286d059eaa
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-02 22:03:38 -0700

    add Header and Footer to app

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Copyright Bikes R Us 2025</p>
+    </footer>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#bikes-index">All Bikes</a> | <a href="#bikes-new">New Bike Entry</a>
+    </header>
+  )
+}
