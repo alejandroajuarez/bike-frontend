a052afc44e877e6db7e423e3134e9aeed7118905
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-25 18:31:08 -0600

    added axios config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import axios from 'axios';
 import './App.css'
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { BikesPage } from './BikesPage';
+
+axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "bike-api-u20x.onrender.com";
 
 function App() {
   return (
